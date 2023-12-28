dfedd7fa063bdc86dac2683e4ed79f11ece9a359
Godfrey Namutagi <godfrenam@yahoo.com>
2023-12-28 11:36:17 +0300

    formatted with black

--- src/IPYNBRENDERER/youtube.py
+++ src/IPYNBRENDERER/youtube.py
@@ -7,48 +7,45 @@ from py_youtube import Data
 
 @ensure_annotations
 def get_time_info(URL: str) -> int:
-    def _verifyvidid_len(vid_id,__expected_len=11):
+    def _verifyvidid_len(vid_id, __expected_len=11):
         len_of_vid_id = len(vid_id)
         if len_of_vid_id != __expected_len:
             raise InvalidURLException(
                 f"Invalid video id with length: {len_of_vid_id}, expected: {__expected_len}"
             )
+
     try:
-        split_val = URL.split('=')
-        if len(split_val) >3:
+        split_val = URL.split("=")
+        if len(split_val) > 3:
             raise InvalidURLException
         if "watch" in URL:
-            if ("&t" in URL):
-                vid_id,time = split_val[-2][:-2], int(split_val[-1][:-1])
+            if "&t" in URL:
+                vid_id, time = split_val[-2][:-2], int(split_val[-1][:-1])
                 _verifyvidid_len(vid_id)
                 logger.info(f"Video starts at: {time}")
                 return time
             else:
-                
-                vid_id,time = split_val[-1], 0
+                vid_id, time = split_val[-1], 0
                 _verifyvidid_len(vid_id)
                 logger.info(f"Video starts at: {time}")
                 return time
         else:
             if "=" in URL and "&t" in URL:
-                
-                vid_id,time = split_val[0].split("/")[-1][:-2],int(split_val[-1])
+                vid_id, time = split_val[0].split("/")[-1][:-2], int(split_val[-1])
                 _verifyvidid_len(vid_id)
                 logger.info(f"Video starts at: {time}")
                 return time
             else:
-                vid_id, time = URL.split("/")[-1],0
+                vid_id, time = URL.split("/")[-1], 0
                 _verifyvidid_len(vid_id)
                 logger.info(f"video starts at: {time}")
                 return time
     except Exception:
         raise InvalidURLException
-                
-    
-                
+
 
 @ensure_annotations
-def render_YouTube_video(URL: str, width: int=780, height: int=160) ->str:
+def render_YouTube_video(URL: str, width: int = 780, height: int = 160) -> str:
     try:
         if URL is None:
             raise InvalidURLException("URL cannot be None")
@@ -72,12 +69,9 @@ def render_YouTube_video(URL: str, width: int=780, height: int=160) ->str:
             """
             display.display(display.HTML(iframe))
             return "success"
-        
+
         else:
             raise InvalidURLException
-        
+
     except Exception as e:
         raise e
-            
-        
-            
